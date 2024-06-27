5ef389cc1f8e654a7105d30b79b9650cbbc245da
Hyeon Chan <116485176+lemonmungchi@users.noreply.github.com>
2024-06-27 15:32:24 +0900

    3강. 장치초기화

--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -11,11 +11,41 @@ public:
 	void Update();
 	void Render();
 private:
+	/// <summary>
+	/// 그리기 함수
+	/// </summary>
+	void RenderBegin();		
+	void RenderEnd();
+private:
+	void CreateDeviceAndSwapChain();
+	/// <summary>
+	/// 버퍼를 묘사 Tag를 달아서 GPU에 알려주기 위함
+	/// </summary>
+	void CreateRenderTargetView();
+	/// <summary>
+	/// 뷰포트 묘사 
+	/// </summary>
+	void SetViewport();
+private:
 	HWND _hwnd;
 	uint32 _width = 0;
 	uint32 _height = 0;
 
 private:
-	//DX
+	//Device & SwapChain
+	// I: 인터페이스 Comptr -> 스마트 포인터- 자동관리해줌, wrl에 있다.
+	ComPtr<ID3D11Device> _device; 
+	ComPtr<ID3D11DeviceContext> _deviceContext;
+	//DXGI : DX와는 독립적으로 하위 수준 작업 관리 => 시스템,하드웨어 통신
+	//스왑체인-> 그리는 것과 보여주는 것 따로 해야 보통 방식 ->전면 후면 고속복사
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
+
+	///RTV 렌더타켓뷰
+	ComPtr<ID3D11RenderTargetView> _renderTargetView;
+
+	//Misc 
+	D3D11_VIEWPORT _viewport = { 0 };
+	float _clearColor[4] = { 0.5f,0.5f,0.5f,0.5f };
+	
 };
 
